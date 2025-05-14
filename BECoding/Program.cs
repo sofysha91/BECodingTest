c47865f7aa407f82b70be9e86b85a7f5fa17e951
sasimplea <sa@simplea.com>
2025-05-13 23:05:41 -0600

    Reading input and getting scores

---
+++ BECoding/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!"); 
+
+class Program
+{
+
+    static void Main()
+    {
+        IEnumerable<string> matches;
+
+        Console.WriteLine("Enter match results:");
+        var matchResults = new List<string>();
+        string match;
+
+        while (!string.IsNullOrWhiteSpace(match = Console.ReadLine()))
+        {
+            matchResults.Add(match);
+        }
+        matches = matchResults;
+        Console.WriteLine(matches);
+
+    }
+
+    public void ProccessScore(List<string> matches)
+    {
+        foreach (var match in matches)
+        {
+            var teams = match.Split(',');
+
+            var team1 = teams[0];
+            var team2 = teams[1];
+        }
+
+    }
+}
+
